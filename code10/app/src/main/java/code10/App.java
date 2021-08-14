16e614bccce021515b4445c42c976e4642e09ceb
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-08-14 14:27:35 +0300

    done with test

--- code10/app/src/main/java/code10/App.java
+++ code10/app/src/main/java/code10/App.java
@@ -65,17 +65,28 @@ public class App {
         enQSh.enqueue(dog);
         enQSh.enqueue(cat);
 
-        System.out.println(enQSh.front.nodeValue.animalName);
+        //System.out.println(enQSh.front.nodeValue.animalName);
         //System.out.println(enQSh.toString());
         //System.out.println(cat.animalName);
 
-        System.out.println(enQSh.dequeue().animalName);
+        //System.out.println(enQSh.dequeue().animalName);
 
        //System.out.println(enQSh.rear.nodeValue);
       //System.out.println(enQSh.toString());
         //Animal cat2=new Animal("CAT2 NAME2");
        // enQSh.enqueue(cat2);
      //System.out.println(enQSh.toString());
+        //code13//
+        StackQueueBrackets<String> newObj=new StackQueueBrackets<>();
+        String brackets="([])";
+        String brackets2="([]";
+        if (newObj.checkStackQueueBrackets(brackets)){
+            System.out.println("Balanced");
+        }else System.out.println("Not Balanced");
+        if (newObj.checkStackQueueBrackets(brackets2)){
+            System.out.println("Balanced");
+        }else System.out.println("Not Balanced");
+
     }
 
 }
