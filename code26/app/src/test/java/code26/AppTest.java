2bc9ef44e602b296249a1cb8bfcfc19791afc5bd
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-08-30 00:09:57 +0300

    done with code 26

--- code26/app/src/test/java/code26/AppTest.java
+++ code26/app/src/test/java/code26/AppTest.java
@@ -4,11 +4,21 @@
 package code26;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+    @Test void testsort(){
+        Sort sort=new Sort();
+        int [] arr={8,4,23,42,16,15};
+        int [] res={4,8,15,16,23,42};
+//        assertEquals(res, Arrays.toString(sort.insertionSort(arr)));
+        assertEquals(Arrays.toString(res), Arrays.toString(sort.insertionSort(arr)));
     }
 }
